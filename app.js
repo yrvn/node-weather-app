97cdab561fd7f90ac2b94e4d61fc6607154a671a
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-14 02:32:13 -0300

    Init

---
+++ app.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+const apikey = 'AIzaSyBCP_ZZpAxf9Nc0gN7vsPZR6ie3Hwp2Ukk';
+var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=Calle%202%20y%20Felipe%20cardoso&apikey=' + apikey;
+
+request({
+    url,
+    json: true
+}, (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2));
+});
