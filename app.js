2ddcac65407111be0af77776b4401786196aa416
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-14 02:57:42 -0300

    Prettier

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const request = require('request');
 
 const apikey = 'AIzaSyBCP_ZZpAxf9Nc0gN7vsPZR6ie3Hwp2Ukk';
-var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=Calle%202%20y%20Felipe%20cardoso&apikey=' + apikey;
+var url =
+    'http://maps.googleapis.com/maps/api/geocode/json?address=Calle%202%20y%20Felipe%20cardoso&apikey=' +
+    apikey;
 
-request({
-    url,
-    json: true
-}, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
-});
+request(
+    {
+        url,
+        json: true
+    },
+    (error, response, body) => {
+        console.log(JSON.stringify(body, undefined, 2));
+    }
+);
