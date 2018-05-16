47117b497e1bf02b27eb1c529e569eede360a074
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-15 23:39:10 -0300

    Lesson30

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var url =
     'http://maps.googleapis.com/maps/api/geocode/json?address=Calle%202%20y%20Felipe%20cardoso&apikey=' +
     apikey;
 
-request(
-    {
+request({
         url,
         json: true
     },
     (error, response, body) => {
-        console.log(JSON.stringify(body, undefined, 2));
+        //console.log(JSON.stringify(error, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
     }
 );
