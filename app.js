2d9083bdc359dbc4b7f48ee1cee4e5a86cfc12d9
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-16 00:15:04 -0300

    Lesson 31

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
 
 const apikey = 'AIzaSyBCP_ZZpAxf9Nc0gN7vsPZR6ie3Hwp2Ukk';
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var address = encodeURIComponent(argv.address);
 var url =
-    'http://maps.googleapis.com/maps/api/geocode/json?address=Calle%202%20y%20Felipe%20cardoso&apikey=' +
+    'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&apikey=' +
     apikey;
 
 request({
