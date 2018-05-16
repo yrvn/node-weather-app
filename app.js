4849099470aaa6d896a89beb083bcde1ee9b5420
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-16 00:20:06 -0300

    Lesson 32

--- app.js
+++ app.js
@@ -21,15 +21,21 @@ var address = encodeURIComponent(argv.address);
 var url =
     'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&apikey=' +
     apikey;
-
+// console.log(url);
 request({
         url,
         json: true
     },
     (error, response, body) => {
-        //console.log(JSON.stringify(error, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
+        if (error) {
+            console.log('Unable to connect to Google servers.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log('Unable to find that address.');
+        } else if (body.status === 'OK') {
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        }
     }
 );
