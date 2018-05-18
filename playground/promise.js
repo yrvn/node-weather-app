e9a64708f3d500f9d65e8b3fc592901d162fb817
Yuri Alvarez <yurialvarez1984@gmail.com>
2018-05-18 00:16:45 -0300

    Lecture 37

---
+++ playground/promise.js
@@ -0,0 +1,39 @@
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(12, '24')
+    .then(res => {
+        console.log('Resultado: ', res);
+        return asyncAdd(res, 33);
+    })
+    .then(res => {
+        console.log('Nuevo resultado : ', res);
+    })
+    .catch(errorMessage => {
+        console.log(errorMessage);
+    });
+
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //resolve('Hey. It worked!');
+//         reject('Unable to fulfill promise');
+//     }, 2500);
+// });
+
+// somePromise.then(
+//     message => {
+//         console.log('Success: ', message);
+//     },
+//     errorMessage => {
+//         console.log('Error: ', errorMessage);
+//     }
+// );
